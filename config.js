867ecce3db077577501362bd0a923a5c2f1c61ff
AkarshanDas <22051489@kiit.ac.in>
2026-02-04 17:41:07 +0530

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dxbtuqirg/video/upload/v1770206986/y2mate.com_-_girl_in_red_we_fell_in_love_in_october_isarcj.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
